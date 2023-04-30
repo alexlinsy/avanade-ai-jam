7c75beeeacea4982f6b9e7968f3b916135c8e0bd
shaoyi.lin <shaoyi.lin@avanade.com>
2023-04-29 22:36:22 -0700

    Change react app to next.js app

---
+++ developers.ts
@@ -0,0 +1,131 @@
+export type DeveloperType = (typeof developers_data)[0];
+
+export const developers_data = [
+  {
+    id: 1,
+    avatar:
+      "https://robohash.org/8283c2129119a4d411a1ad14c7aed375?set=set4&bgset=&size=400x400",
+    name: "Shijing(Brenda) Liu",
+    title: "FrontEnd Development",
+    level: "Sr. Analyst",
+    contact: "NA Engineering Hubs Avanade, Inc.",
+    work_email: "shijing.liu@avanade.com",
+    region: "central Canada",
+    country: "Canada",
+    city: "Winnipeg",
+    embedding: 1234,
+    experiences: [
+      {
+        company:
+          "Large US Mortgage Company - Full-stack developer - 1.8 years.",
+        content:
+          "Html,css,js.Programmed new features for the company’s webapp, frontend and backend by using angular, c# or mySQL as tech stack.\n Creating many useful articles sharing with our feature team and big team to improve the work performance and workflow.\n Discussion and communicate with PMs, BAs, QAs, Scrum Masters to clarify their concerns and target to the right solution. Getting lots of good feedbacks from client side.\n Resolving support tickets on a regular basis responsible for release support team.\n Creating API and wring reusable SQL scripts to implement the feature or fixing the database bugs\n Helping new team joiners setting up their local environment and helping with their questions.",
+      },
+      {
+        company:
+          "Large US Manufacturing Company - Full-stack developer – 5 months",
+        content:
+          "React, Angular, NodeJs, Azure DevOps, MySQL. Git,ASP.Net. Programmed new features for two of the company’s management webapps, frontend and backend by ASP.Net entity framework and MYSQL as tech stack. \n Writing instructions and technical document about how to use the app and app features’ etc.\n Work UI/UX roll to build the app outlook and got great feedback about it. \n Presenting features to the client and answer their questions. Writing script to migration the data to SQL server. \n Implementing the app’s frontend by React.\n Join the project planning with our team tech manager, developers and BAs, providing suggestions.",
+      },
+      {
+        company:
+          "Large E-Commerce company - App Integration Specialist – 2.5 years",
+        content:
+          "React, liquid, Shopify, JS.Integration apps on E-Commerce stores. Fixing bugs, customize Shopify themes. Writing apps integration’s technical articles on specific themes and share them on company’s confluence platform.\n Attend company internal tools webapp’s UI/UX research and design. \nTraining teammates and other department developers about app integration. \nCommunicate with tier 1 custom support and app development team, deliver issues and suggestions. ",
+      },
+    ],
+    certificates: "Azure-900, Azure AI-900, Azure-204, AZ-400",
+  },
+  {
+    id: 2,
+    avatar:
+      "https://robohash.org/3ad5fb24cb35f030459c72b23fe96fa6?set=set4&bgset=&size=400x400",
+    name: "Shaoyi(Alex) Lin",
+    title: "Full-stack Development",
+    level: "Consultant",
+    contact: "NA Engineering Hubs Avanade, Inc.",
+    work_email: "shaoyi.lin@avanade.com",
+    region: "central Canada",
+    country: "Canada",
+    city: "Vancouver",
+    embedding: 1234,
+    experiences: [
+      {
+        company: "CIBC Online Banking",
+        content:
+          "Vue, Ember, JavaScript, CSS, VS Code, Git.Leaded full-stack development team for Adobe data analytics project with 3 successful web releases, includes more than 20 Vue and Ember web applications.Established project control procedures with more than 10 guideline documentations to ensure the efficiency and quality of project deliverables.Mentor teammates to get familiar with the project and solve frontend technical problems.",
+      },
+      {
+        company: "Large Airline Company",
+        content:
+          "React, Redux, JavaScript, CSS, JSX, VS Code, Figma, Git Responsible for building UI and booking system for the airline company in a team of four developers. Followed agile development and TDD to ensure the efficiency and quality of the application. Accomplished comprehensive data process with REST API and Redux.",
+      },
+      {
+        company: "Large hotel brand in Australia",
+        content:
+          "React Native, Redux, JavaScript, CSS, JSX, VS Code, Figma, Git.Contributed to the mobile application team for building the IOS and Android application for the large hotel brand of Australia, utilizing react native, redux and various of react native Responsible for updating Figma UI designs to mobile apps and fixing bugs.",
+      },
+      {
+        company: "Large railroad company in US",
+        content:
+          "React, Redux, JavaScript, CSS, JSX, VS Code, Figma, Git.Built agent sales portal for a US large rail bookers brand in a team of six developers, utilizing react, redux, react testing library and with multiple react libraries to optimize functionalities.Followed agile development guide into daily scrum, meeting and planning. Created peer reviews for each PR to ensure the quality of code.Created unit test for each component to reduce bugs and improve functionalities of the application.",
+      },
+    ],
+    certificates: "AZ-900, AZ-204, AI-900",
+  },
+  {
+    id: 3,
+    avatar:
+      "https://robohash.org/3ad5fb24cb35f030459c72b23fe96fa6?set=set4&bgset=&size=400x400",
+    name: "John Doe",
+    title: "Back end Development",
+    level: "Consultant",
+    contact: "NA Engineering Hubs Avanade, Inc.",
+    work_email: "john.doe@avanade.com",
+    region: "West US",
+    country: "US",
+    city: "Seattle",
+    embedding: 1234,
+    experiences: [
+      {
+        company: "BOA Online Banking",
+        content:
+          "Java, Git. Leaded back end development team for Adobe data analytics project with 5 successful web releases, includes more than 20 Java applications. Mentor teammates to get familiar with the project and solve database problems.",
+      },
+      {
+        company: "Large Financial Corporation",
+        content:
+          "Expreince of developing and handling database for large airline company. Followed agile development and TDD to ensure the efficiency and quality of the application. Accomplished comprehensive data process with Java, SQL and Spring Boot.",
+      },
+    ],
+    certificates: "AZ-900, AZ-204, DB-900",
+  },
+  {
+    id: 4,
+    avatar:
+      "https://robohash.org/5a9fcaabf7d864d163424a7408cc16f1?set=set4&bgset=&size=400x400",
+    name: "Jane Smith",
+    title: "Mobile Application Development",
+    level: "Senior Developer",
+    contact: "ABC Technologies",
+    work_email: "jane.smith@abc.com",
+    region: "East US",
+    country: "US",
+    city: "New York",
+    embedding: 5678,
+    experiences: [
+      {
+        company: "XYZ Mobile",
+        content:
+          "Designed and developed a mobile app using React Native for both iOS and Android platforms. Worked on app features like geolocation, push notifications, in-app purchases, and social media integration. Led a team of 5 developers and conducted code reviews for quality assurance.",
+      },
+      {
+        company: "PQR Bank",
+        content:
+          "Developed a mobile banking app using Java, Kotlin and Android Studio. Implemented security features like fingerprint authentication, PIN lock, and encryption for data protection. Worked with RESTful APIs for data exchange and collaborated with UI/UX designers for app design.",
+      },
+    ],
+    certificates:
+      "AWS Certified Developer - Associate, Google Associate Android Developer, Oracle Mobile Application Framework 2 Developer Certified Associate",
+  },
+];
